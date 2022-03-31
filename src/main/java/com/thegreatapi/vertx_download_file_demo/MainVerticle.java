d35a740bad48ee48726e038f0defc8be1caa8a1f
Helber Belmiro <helber.belmiro@gmail.com>
2022-03-31 07:47:37 -0300

    Changed to use sendFile
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/main/java/com/thegreatapi/vertx_download_file_demo/MainVerticle.java
+++ src/main/java/com/thegreatapi/vertx_download_file_demo/MainVerticle.java
@@ -2,36 +2,30 @@ package com.thegreatapi.vertx_download_file_demo;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.buffer.Buffer;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
+import java.net.URL;
 
 import static java.lang.System.Logger.Level.ERROR;
 import static java.lang.System.Logger.Level.INFO;
 
 public class MainVerticle extends AbstractVerticle {
 
-    private static System.Logger LOGGER = System.getLogger(MainVerticle.class.getName());
+    private static final System.Logger LOGGER = System.getLogger(MainVerticle.class.getName());
 
     private static final String FILE_NAME = "/dummy.pdf";
 
     @Override
     public void start(Promise<Void> startPromise) {
         vertx.createHttpServer().requestHandler(req -> {
-            try (InputStream file = getClass().getResourceAsStream(FILE_NAME)) {
-                if (file == null) {
-                    LOGGER.log(ERROR, "File not found");
-                    req.response().setStatusCode(404).end();
-                } else {
-                    req.response()
-                            .putHeader("Content-Disposition", "inline; filename=\"dummy.pdf\"")
-                            .putHeader("Content-Type", "application/octet-stream")
-                            .end(Buffer.buffer(file.readAllBytes()));
-                }
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
+            URL file = getClass().getResource(FILE_NAME);
+            if (file == null) {
+                LOGGER.log(ERROR, "File not found");
+                req.response().setStatusCode(404).end();
+            } else {
+                req.response()
+                        .putHeader("Content-Disposition", "inline; filename=\"dummy.pdf\"")
+                        .putHeader("Content-Type", "application/octet-stream") // force download (instead of open in browser)
+                        .sendFile(file.getPath());
             }
         }).listen(8888, http -> {
             if (http.succeeded()) {
